c1e81f42e2c1718fb3144577796dd913a74131de
Matthew <mattfitzgerald85@gmail.com>
2018-10-12 15:44:18 -0400

    Added Transaction class.
    Assigns An ArrayList of transactions to each account.

---
+++ src/Transaction.java
@@ -0,0 +1,19 @@
+/**
+ * A Transaction class which consists of a transaction type 
+ * (e.g., create, deposit, withdrawal, balance, etc.) and a
+ *  transaction amount (where applicable)
+ */
+
+/**
+ * @author Matthew
+ *
+ */
+public class Transaction {
+
+	public Transaction() {
+		
+	}
+	public Transaction(String a) {
+		
+	}
+}
